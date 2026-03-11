9e8e64151eb0066e77dca0a0d83329b276d377fe
keyurgovanitops <keyurgovani.tops@gmail.com>
2026-03-11 12:15:17 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/*
+// javascript :- it is a scripting language. to make websites dynamic.
+
+// high-level:-developer friendly low-level- complex or closer to hardware
+
+// js is dynamic languege - static languge - c, c++, java
+static - data-type -> can not change data-type at runtime
+dynamic - data-type -> can change at runtime
+
+
+
+// compiler - c || javascript - interpreter/engine - v8
+// compiler -> program -> machine code 
+// interpreter -> line by line
+
+// client-side-scripting - server side-scripting
+// run time enviornment - Node
+// client-side - logic - click, styling, request
+// server-side - logic - request ko handle - response
+
+// DOM manipulation - document object model
+
+// vanilla js
+
+
+
+
+*/
