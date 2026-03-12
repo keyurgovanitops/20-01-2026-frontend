86296f39088323a460e3bf5b263cb900c447676f
keyurgovanitops <keyurgovani.tops@gmail.com>
2026-03-12 12:13:41 +0530

    Add files via upload

---
+++ js/variable.js
@@ -0,0 +1,97 @@
+// variable - a variable is a named storage location in memory that can hold a value.
+
+// RAM - volatile, temperory
+
+// let, const - block scoped
+// var - function scoped
+
+// keyword variable value
+// let a; // declaration
+// a=5; // assignment
+
+// var hu = 6;
+// // function definition
+// function ab(){
+//     hu = 7;
+//     console.log(hu);
+// } 
+
+// ab(); // function calling
+// console.log(hu);
+// // const c = 9;
+// // c=8;
+
+// {
+//     let a = 5;
+//     var b = 6;
+//     const c = 6;
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// function a(){
+//     let a = 5;
+//     {
+//         var b = 7;
+//     }
+//     // console.log(b);
+// }
+// a();
+// console.log(b);
+
+
+// let a = 8; // reassign - yes | redeclare - no
+// const b = 9; // reassign - no | redeclare - no
+// var c = 0; // reassign - yes | redeclare - yes
+
+// console.log(c);
+// var c = 8;
+
+// console.log(c);
+
+
+// function - reusablity - execution - depend on call
+// loops - repetation - iterate - depends on condition
+
+// let b = 3;
+// let obj = {
+//     name : 'rahul'
+// }
+// let c = true;
+// let a = "string";
+// let d = [1,2,3,4, "string"];
+// let e = undefined; // mistake by programmer or by js itself when variable is declared but not assigned any value.
+// let f = null; // intentional  - in future the value will be assigned.
+// primitive :- boolean, number, undefined, null
+// non-primitive:- array, object, function
+// // object - key value pair
+// let g = {
+//     name: "john", 
+//     age: 30, 
+//     city: "new york"
+// };
+
+// let count= 0;
+// function sayHi(){
+//     count = count + 1;
+//     if(count > 10){
+//         return;
+//     }
+//     console.log(count + " hi");
+//     sayHi();
+// }
+// sayHi();
+// sayHi();
+// sayHi();
+// sayHi();
+// console.log(typeof(b));
+// console.log(typeof(a));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+// console.log(g["age"]);
+// console.log(g.name);
+
