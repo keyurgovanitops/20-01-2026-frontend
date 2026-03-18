2a86a05f6df8ca743215145eb70765442af5c5a3
keyurgovanitops <keyurgovani.tops@gmail.com>
2026-03-18 12:07:28 +0530

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,56 @@
+// function :- it is a block of code that can be reused
+
+// keyword function-name (parameter) {body of function}
+// function prem(d,name,g, h){
+//     console.log("hi "+ name + d + g + h);
+// }
+
+// prem(10,20,30,40);
+
+
+// function add(a,b){
+//     return a + b;
+// }
+// let total = add(10,20);
+// console.log(add(10,20));
+
+// function add1(a,b){
+//     console.log(a+b);
+// }
+
+// let a = add1(10,20);
+// console.log('a = ' + a);
+
+// function makeAlert(){
+//     confirm("Are you insane?")
+// }
+
+// makeAlert();
+
+// function makeAlert(){
+//     confirm("You are insane.")
+// }
+
+// makeAlert();
+
+// function makeAlert(){
+//     let a = prompt("welcome to website")
+//     console.log(a);
+// }
+
+// makeAlert();
+
+// anonymous function - function which doesn't have name 
+
+// let a = function (){
+//     console.log("hi");
+// } // function expression
+
+// a();
+// console.log(a);
+
+// let b = () => {
+//     console.log("hello");
+// } // arrow function
+
+// b();
