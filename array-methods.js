67bc2bbf7044ea2df611d1fb3c767710b914f8a7
keyurgovanitops <keyurgovani.tops@gmail.com>
2026-03-20 12:06:27 +0530

    Add files via upload

---
+++ array-methods.js
@@ -0,0 +1,25 @@
+// 
+// function addition(a,b){
+//     return a+b;
+// }
+
+// let fn = (a,b)=>a+b;
+            
+
+// console.log(fn(5,8));
+
+// let a = [1,2,3,4,5];
+
+// foreach doesn't return anything and does not change original array.
+// let returnValue = a.forEach((item,index)=>console.log(index, item));
+// console.log(returnValue);
+
+
+// let a = [1,2,3,4,5];
+// console.log('before map',a);
+
+// map does return new array and does not change original array.
+// let mapValue = a.forEach((item)=>item*2);
+
+console.log('after map',a);
+console.log(mapValue);
---
+++ hof&cb.js
@@ -0,0 +1,16 @@
+// higher order function :- a function that takes another function as argument.
+// call back function :- a function that gets passed as argument is called cb fn.
+// function ab(ab){
+//     ab("mohit","prem");
+// }
+// // let a = 'string';
+// // let b = "string";
+// // let c = `this is also string` // template literal
+// function b(name,name2){
+//     console.log(`hi ${name2} ${name}`);
+// }
+
+// ab(b);
+
+
+
