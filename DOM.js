7d4a3250b070baadb5fca93e18997286f28cd890
keyurgovanitops <keyurgovani.tops@gmail.com>
2026-03-23 12:16:58 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,17 @@
+// let divElement = document.getElementsByTagName("button");
+// let divElement1 = document.getElementById("id");
+// let divelement2 = document.querySelector(".btn");
+// let divelemnt3 = document.getElementsByClassName("classname");
+// let divelement4 = document.querySelectorAll(".btn");
+
+// console.log(divElement[0]);
+
+// divElement[0].addEventListener("click", product1Clicked);
+
+// function product1Clicked(){
+//         console.log("product 1 clicked");
+// }
+
+// window.addEventListener('wheel', (event) => {
+//   console.log("wheel moved")
+// });
--- array-methods.js
+++ array-methods.js
@@ -4,7 +4,6 @@
 // }
 
 // let fn = (a,b)=>a+b;
-            
 
 // console.log(fn(5,8));
 
@@ -21,5 +20,32 @@
 // map does return new array and does not change original array.
 // let mapValue = a.forEach((item)=>item*2);
 
-console.log('after map',a);
-console.log(mapValue);
+// console.log('after map',a);
+// console.log(mapValue);
+
+
+// let a = [1,2,3,4,5];
+// console.log('before filter',a);
+
+// // map does return new array and does not change original array.
+// a = a.filter((item,index)=>{
+//     return item > 3;
+// });
+
+
+// console.log('original array:- ',a);
+// // console.log('filteredValue' , filteredValue);
+
+
+// let a = [5,2,3,9,5];
+// console.log('before find',a);
+
+// // find does returns a first instance of value that passes the condition and does not change original array.
+// let foundValue = a.find((item)=>{
+//     return item > 3;
+// });
+
+
+// console.log('original array:- ',a);
+// console.log('foundValue' , foundValue);
+
